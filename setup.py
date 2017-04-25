4647d99248f1bbedb72b050d34b0d2c58dbb6d55
Philippe <phil.oger@gmail.com>
2017-04-25 18:04:31 +0200

    maj version

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ def read(fname):
 
 setup(
     name="twitter-analytics",
-    version="0.0.1",
+    version="0.0.6",
     author="Philippe Oger",
     author_email="phil.oger@gmail.com",
-    description=("A twitter analytics reports downloader. The only way to get tweet impressions data"),
+    description=("A twitter analytics reports downloader. The only way to get tweet impressions data with Python"),
     license="MIT",
     keywords="twitter analytics reports downloader",
     url="https://github.com/philippe2803/twitter-analytics-wrapper",
@@ -21,5 +21,5 @@ setup(
         'python-dateutil',
         'pyvirtualdisplay'
     ],
-    long_description=read('README.md')
+    long_description=read('README')
 )

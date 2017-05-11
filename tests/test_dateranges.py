1de2c0f908be7739fad4af090f27d8a638719c2a
Philippe <phil.oger@gmail.com>
2017-05-11 15:07:54 +0200

    fix

--- tests/test_dateranges.py
+++ tests/test_dateranges.py
@@ -5,5 +5,4 @@ def test_split_date_ranges():
     from_date = '01/01/2015'
     to_date = '02/28/2015'
     date_ranges = ReportDownloader.split_date_range_into_91(from_date, to_date)
-    print(date_ranges)
-    assert len(date_ranges) == 0
+    assert len(date_ranges) == 1
